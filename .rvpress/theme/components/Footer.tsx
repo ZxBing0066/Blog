214bea39b152280179b1dcb0eca55537673a2a03
ZxBing0066 <ZxBing0066@gmail.com>
2022-04-18 09:16:17 +0800

    fix image path

--- .rvpress/theme/components/Footer.tsx
+++ .rvpress/theme/components/Footer.tsx
@@ -6,6 +6,13 @@ export default React.memo(() => {
     return (
         <footer className={'footer ' + cls.footer}>
             <div className='container'>
+                <p className='copyright'>
+                    © 2014-present{' '}
+                    <a href='https://github.com/ZxBing0066' target='_blank'>
+                        ZxBing0066
+                    </a>
+                    . All Rights Reserved.
+                </p>
                 <p className='powered'>
                     Powered by{' '}
                     <a href='https://github.com/ZxBing0066/rvpress' target='_blank'>
